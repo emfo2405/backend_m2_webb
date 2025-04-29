a937b6772d54b69abd1a96fa9bfcd5ff0edde537
emfo2405 <emfo2405@student.miun.se>
2025-04-29 18:20:30 +0200

    Skapat funktion som ska föra data från formulär till databas

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
 async function addExperience() {
-    
+
+    let companyName = document.getElementById("companyname");
+    let jobTitle = document.getElementById("jobtitle");
+    let location = document.getElementById("location");
+    let startDate = document.getElementById("startdate");
+    let endDate = document.getElementById("enddate");
+    let description = document.getElementById("description");
+
+    let experience = {
+        companyname: companyName.value,
+        jobtitle: jobTitle.value,
+        location: location.value,
+        startdate: startDate.value,
+        enddate: endDate.value,
+        description: description.value
+    }
+
+    let response = await fetch('http://localhost:3000/api/experience', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(experience)
+    });
+
+    let data = await response.json();
+    console.log(data);
 }
+
+let submitButton = document.getElementById("submit-button");
+
+submitButton.onclick = addExperience();
