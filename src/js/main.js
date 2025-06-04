ac2e7515b730b3761009cc5136c44e3108d0cf87
emfo2405 <emfo2405@student.miun.se>
2025-06-04 07:51:26 +0200

    Rätt länk till databas

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,7 @@ async function addExperience() {
     }
 
     //Kopplar till API och lägger till ny data i databas
-    let response = await fetch('https://backend-m2-webb.onrender.com/api/experience', {
+    let response = await fetch('https://backend-m2.onrender.com/api/experience', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
